a9e2027c4e2102ee8b173aa3102716ee1d7d916f
ngadiono <ondoelratz@gmail.com>
2018-07-17 02:58:07 +0700

    add

--- src/index.js
+++ src/index.js
@@ -3,23 +3,33 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCa31HMF_a9L3dbZ69LPz_ngYskk268vuo';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
     YTSearch({key: API_KEY, term: 'cr7'}, (videos) => {
-      this.setState({videos});
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render() {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos}/>
+      <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
       </div>
     )
   }
